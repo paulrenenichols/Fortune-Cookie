9eb41fdbc26c0842e49d78b320a4464fdbf290c9
paul rene nichols <paul.rene.nichols@gmail.com>
2013-01-12 15:29:06 -0600

    Added comments to strip_newlines.sh bash script
    I didn't mention this script in the last commit, so I added comments to
    document its functions in order to mention the script in a commit
    message.
    
    This script exists to convert human readable design documents into
    valid JSON that can be sent to a CouchDB database.

--- CouchDB/fortunes/DesignDocuments/scripts/strip_newlines.sh
+++ CouchDB/fortunes/DesignDocuments/scripts/strip_newlines.sh
@@ -1,1 +1,5 @@
+#Bash script that strips newline characters from file specified by first command line argument,
+#  then stores results in file specified by second command line argument.
+#
+#Used for converting human readable CouchDB design document into one which is valid JSON
 sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' $1 > $2
